467f6e8a9378466eeb25447dfdbe9eb673f8f52e
Lukasz Gornicki <derberg@wp.pl>
2019-11-18 20:08:50 +0100

    Fix helm installation

--- rafter/install-rafter.sh
+++ rafter/install-rafter.sh
@@ -1,6 +1,10 @@
 echo "Helm chart cloning"
 git clone https://github.com/magicmatatjahu/rafter.git
-cd charts
+cd rafter
+git checkout chart
+
+echo "Helm chart setup"
+helm init
 
 echo "Helm chart installation"
-helm install --name rafter rafter-charts/rafter --verbose
+helm install --name rafter ./charts/rafter --debug
