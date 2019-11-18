13eec77eeb4f2f2751269ece0b70cb277879bbde
Lukasz Gornicki <derberg@wp.pl>
2019-11-18 19:40:40 +0100

    Remove helm installation as it is already part of the env

--- rafter/install-rafter.sh
+++ rafter/install-rafter.sh
@@ -1,11 +1,6 @@
-echo "Helm chart installation"
+echo "Helm chart cloning"
 git clone https://github.com/magicmatatjahu/rafter.git
 cd charts
 
-#Install helm
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
-$ chmod 700 get_helm.sh
-$ ./get_helm.sh
-
-#Install chart
-helm install --name rafter rafter-charts/rafter
+echo "Helm chart installation"
+helm install --name rafter rafter-charts/rafter --verbose
