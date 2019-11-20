f77235c4519b56d5c88b5a20d3e65e2c910892ae
Lukasz Gornicki <derberg@wp.pl>
2019-11-20 18:04:13 +0100

    Add helm update script

--- rafter/install-rafter.sh
+++ rafter/install-rafter.sh
@@ -1,4 +1,5 @@
-#echo "Helm chart cloning"
-#git clone https://github.com/magicmatatjahu/rafter.git
-#cd rafter
-#git checkout chart
+#echo "Upgrade to Helm 1.16.0"
+curl -LO https://get.helm.sh/helm-"v2.16.0"-linux-amd64.tar.gz
+tar -xzvf helm-"v2.16.0"-linux-amd64.tar.gz
+mv ./linux-amd64/{helm,tiller} /usr/local/bin
+helm version
